97aefffab25db81fa319634b6e3c4627fc2b3afa
felosque <felipelondono4@gmail.com>
2020-05-30 16:38:32 -0500

    Borrado Machinery y RequestAnalysis por errores con el proyecto

--- src/main/java/com/crmunibague/crmunibague/machinery/MachineryRepository.java
+++
@@ -1,8 +0,0 @@
-package com.crmunibague.crmunibague.machinery;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-
-public interface MachineryRepository extends JpaRepository<Machinery, Integer> {
-
-}
