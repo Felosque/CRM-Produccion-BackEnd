c2f3cbcede5c7e8dea137b5380182c24a1ff163e
Javier Collazos <jcollazosgomez@gmail.com>
2020-06-02 10:23:47 -0500

    Fixed BranchOffice Service

--- src/main/java/com/crmunibague/crmunibague/branchoffice/BranchOfficeImpl.java
+++ src/main/java/com/crmunibague/crmunibague/branchoffice/BranchOfficeImpl.java
@@ -12,7 +12,6 @@ import java.util.List;
 public class BranchOfficeImpl implements BranchOfficeService{
 
     private BranchOfficeRepository branchOfficeRepository;
-
     private StateBranchOfficeService stateBranchOfficeService;
 
     @Autowired
--- src/main/java/com/crmunibague/crmunibague/branchoffice/BranchOfficeRESTController.java
+++ src/main/java/com/crmunibague/crmunibague/branchoffice/BranchOfficeRESTController.java
@@ -1,9 +1,5 @@
 package com.crmunibague.crmunibague.branchoffice;
 
-<<<<<<< HEAD
-=======
-import com.crmunibague.crmunibague.statebranchoffice.StateBranchOffice;
->>>>>>> Javico_classes
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -12,34 +8,20 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-<<<<<<< HEAD
-@RequestMapping("scm-unibague/branchoffice")
-=======
 @RequestMapping("scm-unibague/BranchOffice")
->>>>>>> Javico_classes
 @RestController
 public class BranchOfficeRESTController {
 
     private BranchOfficeService branchOfficeService;
 
     @Autowired
-<<<<<<< HEAD
-    public BranchOfficeRESTController(BranchOfficeService branchOfficeService)
-    {
-=======
     public BranchOfficeRESTController(BranchOfficeService branchOfficeService) {
->>>>>>> Javico_classes
         this.branchOfficeService = branchOfficeService;
     }
 
     @PostMapping
-<<<<<<< HEAD
-    public ResponseEntity<BranchOffice> save(@RequestBody BranchOffice branchOffice){
-        return ResponseEntity.ok().body(this.branchOfficeService.save(branchOffice));
-=======
     public ResponseEntity<BranchOffice> save(@RequestBody BranchOffice entity){
         return ResponseEntity.ok().body(this.branchOfficeService.save(entity));
->>>>>>> Javico_classes
     }
 
     @GetMapping
@@ -48,11 +30,7 @@ public class BranchOfficeRESTController {
     }
 
     @GetMapping(path = "/{id}")
-<<<<<<< HEAD
-    public ResponseEntity<BranchOffice> getByID(@PathVariable("id") int id){
-=======
     public ResponseEntity<BranchOffice> getById(@PathVariable("id") int id){
->>>>>>> Javico_classes
         return ResponseEntity.ok().body(this.branchOfficeService.getById(id));
     }
 
@@ -60,24 +38,11 @@ public class BranchOfficeRESTController {
     public Map<String, Boolean> delete(@PathVariable("id") int id){
         this.branchOfficeService.delete(id);
         Map<String, Boolean> response = new HashMap<>();
-<<<<<<< HEAD
-        response.put("Delete", Boolean.TRUE);
-=======
         response.put("Deleted", Boolean.TRUE);
->>>>>>> Javico_classes
         return response;
     }
 
-    @PutMapping(path = "/{id}")
-<<<<<<< HEAD
-    public ResponseEntity<BranchOffice> update(@PathVariable int id, @RequestBody BranchOffice branchOffice){
-        return ResponseEntity.ok().body(this.branchOfficeService.update(id, branchOffice));
-    }
-
-
-=======
     public ResponseEntity<BranchOffice> update(@PathVariable("id") int id, @RequestBody BranchOffice entity){
         return ResponseEntity.ok().body(this.branchOfficeService.update(id,entity));
     }
->>>>>>> Javico_classes
 }
