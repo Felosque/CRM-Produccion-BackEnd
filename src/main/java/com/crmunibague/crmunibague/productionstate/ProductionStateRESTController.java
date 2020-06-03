df8e1ba464d0411277f8d5c00fef281d6047d63c
Javier Collazos <jcollazosgomez@gmail.com>
2020-06-03 15:41:05 -0500

    Adding @Crossorigin

--- src/main/java/com/crmunibague/crmunibague/productionstate/ProductionStateRESTController.java
+++ src/main/java/com/crmunibague/crmunibague/productionstate/ProductionStateRESTController.java
@@ -8,6 +8,7 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+@CrossOrigin(origins = "http://localhost:4200",maxAge = 3600)
 @RequestMapping("scm-unibague/ProductionState")
 @RestController
 public class ProductionStateRESTController {
--- src/main/java/com/crmunibague/crmunibague/statebranchoffice/StateBranchRESTController.java
+++ src/main/java/com/crmunibague/crmunibague/statebranchoffice/StateBranchRESTController.java
@@ -8,6 +8,7 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+@CrossOrigin(origins = "http://localhost:4200",maxAge = 3600)
 @RequestMapping("scm-unibague/StateBranchOffice")
 @RestController
 public class StateBranchRESTController {
